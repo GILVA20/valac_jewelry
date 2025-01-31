44546ee4245941b3393959422eb3ac21222523ea
Gil Valencia <gilberto.valencia1234@gmail.com>
2025-01-31 00:24:26 -0600

    Cambios finales paleta y estilo letra

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./static/**/*.js"  // Ajusta según donde tengas tus archivos JS si los usas.
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+        colors: {
+          'cartier-gold': '#A67C00',
+          'dark-gray': '#333333',
+          'light-gray': '#B0B0B0',
+          'white': '#FFFFFF',
+          'black': '#000000',
+        },
+    },
+},
   plugins: [],
 }
 
